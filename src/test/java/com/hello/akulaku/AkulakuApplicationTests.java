9c6b413847fdeeeeb736b9c0322748b1f329e702
riqimaruq <viqiagung@gmail.com>
2024-11-23 17:38:11 +0700

    first commit

---
+++ src/test/java/com/hello/akulaku/AkulakuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.akulaku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkulakuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
